f0f642b493ce42c7fc88edfe7ea3b23d4510477b
jinseong <wlstjd34@gmail.com>
2024-06-10 23:48:18 +0900

    add resource.ts of auth/post-confirmation

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -2,4 +2,8 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
+  entry: './handler.ts',
+  environment: {
+    TEST_ENV: "TEST-ENV"
+  }
 });
