22091243b028e09c27f4adc7fdf10f70ddca597f
jinseong <wlstjd34@gmail.com>
2024-06-11 22:31:34 +0900

    backend cdk 추가

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,26 @@
 import { defineBackend } from '@aws-amplify/backend';
+import * as iam from "aws-cdk-lib/aws-iam"
+import * as sns from "aws-cdk-lib/aws-sns"
 import { data } from './data/resource';
 import { runbatch } from './functions/runbatch/resource'
 
-defineBackend({
+const backend = defineBackend({
   runbatch,
   data,
 });
+
+const runBatchLambda = backend.runbatch.resources.lambda;
+
+const dynamoStack = backend.createStack("dynamoStack")
+const dynamo = new sns.Topic(dynamoStack, "Topic", {
+  displayName: "dynamo",
+})
+
+const statement = new iam.PolicyStatement({
+  sid: "AllowPublish",
+  actions: ["sns:Publish"],
+  resources: [dynamo.topicArn],
+})
+
+
+runBatchLambda.addToRolePolicy(statement);
