e129d2acca98799ecf18c5507f6aaf1b4fd3c02b
jinseong_oh <wlstjd34@gmail.com>
2024-06-11 18:31:42 +0900

    add postConfirmation on aut

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -16,7 +17,7 @@ const schema = a.schema({
       housingType: a.integer(),
       dongCode: a.integer(),
     })
-    .authorization(allow => [allow.publicApiKey()])
+    .authorization(allow => [allow.resource(postConfirmation)])
 });
 
 export type Schema = ClientSchema<typeof schema>;
